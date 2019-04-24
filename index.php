bfcfccd35f958aeafaacfe94fecd674fb10d3202
fmasouza <fabmacsouza@gamil.com>
2019-04-24 16:57:28 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,12 +8,23 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
-$root = new Usuario();
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(3);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
 
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("fabio","1234");
+echo $usuario;
 
  ?>
